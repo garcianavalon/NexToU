6d2f7f6bbce65b1c18539ccbf2f617624c4f7684
Enrique Garcia <garcianavalon@gmail.com>
2014-05-28 16:54:37 +0800

    Added generic contact application
    At some point it will be expanded to be reused in other projects, for
    now just the kind of contact we need for this app

--- NexToU/urls.py
+++ NexToU/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from emailcontactform import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'NexToU.views.home', name='home'),
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^contact/', views.BasicContactView.as_view(),name='contact'),
 )
