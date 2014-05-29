6b77f04d89824b04710dba55d9b65e45011eb98c
Kevin <ck520702002@gmail.com>
2014-05-29 10:38:08 +0800

    modified models.py

--- activitynetwork/models.py
+++ activitynetwork/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User  
+from django.utils.translation import ugettext as _  
+from userena.models import UserenaBaseProfile 
+  
+class MyProfile(UserenaBaseProfile):
+    user = models.OneToOneField(User,
+    							unique=True,
+    							verbose_name=_('user'),
+    							related_name='my_profile') 
+    description = models.CharField(max_length=30)   
+    resume = models.CharField(max_length=30) 
+    #talents  = models.CharField(max_length=30)
+    def __unicode__(self):
+    	return user
