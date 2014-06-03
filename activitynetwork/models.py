b791a2da2ffa6704cca84e2d34758a805a740f73
Kevin <ck520702002@gmail.com>
2014-06-03 15:10:57 +0800

    Merge branch 'more-models' of https://github.com/garcianavalon/NexToU into more-models
    Conflicts:
    	activitynetwork/models.py

--- activitynetwork/models.py
+++ activitynetwork/models.py
@@ -3,7 +3,23 @@ from django.db import models
 class Need(models.Model):
     description = models.CharField(max_length=30)
     def __str__(self):              # __unicode__ on Python 2
+
         return self.description
+class Schedule(models.Model):
+    datetime = models.DateTimeField('schedule datetime')
+    description = models.CharField(max_length=30)
+    def __str__(self):
+        return self.description
+        
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+
+        
+        
 
 class Activity(models.Model):
     #act_holder = models.ForeignKey(MyProfile)
@@ -12,6 +28,10 @@ class Activity(models.Model):
     datetime = models.DateTimeField('activity date')
     description = models.CharField(max_length=30)
     needs = models.ManyToManyField(Need)
+    schedule = models.ForeignKey(Schedule)
+    category = models.ForeignKey(Category)
     def __str__(self):              # __unicode__ on Python 2
         return self.name
+
+
 
