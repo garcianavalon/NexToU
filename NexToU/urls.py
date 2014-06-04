f3b6983754f6696b1c443353f56c2f597d6b2228
Enrique Garcia <garcianavalon@gmail.com>
2014-06-04 18:08:34 +0800

    Templates for signin and signup

--- NexToU/urls.py
+++ NexToU/urls.py
@@ -5,11 +5,18 @@ admin.autodiscover()
 
 #from emailcontactform import views
 from activitynetwork import views as act_views
-
+from accounts.forms import CustomSignupForm, CustomSigninForm
+from userena import views as userena_views
 urlpatterns = patterns('',
 
     #url(r'^$', views.IndexView.as_view(), name='index'), #Index, first page
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/signup/$',
+        userena_views.signup,
+        {'signup_form': CustomSignupForm}),
+    url(r'^accounts/signin/$',
+        userena_views.signin,
+        {'auth_form': CustomSigninForm}),
     url(r'^accounts/', include('userena.urls')),
     url(r'^activities/create/$',act_views.ActivityCreateView.as_view()),
     url(r'^activities/list/$',act_views.ActivityListView.as_view()),
