e811ea7d6b5fb83cd367cd68f59036135b5c3a2c
Enrique Garcia <garcianavalon@gmail.com>
2014-06-05 17:08:23 +0800

    more fields for the profile

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
+from activitynetwork.models import Activity
 
 class VolunteerProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
                                 related_name='volunteer_profile')
-    name = models.CharField(_('name'),
-                                max_length=20)
+    intro = models.CharField(_('intro'),
+                                max_length=140)
+    resume = models.CharField(_('resume'),
+                                max_length=1000)
+    talents = models.CharField(_('talents'),
+                                max_length=300)
+
+    participated_activities = models.ManyToManyField(Activity)
