7e0252fcedb1c744d73b73b9d3cf1ef5cedcc972
ck520702002 <ck520702002@gmail.com>
2014-06-07 02:48:04 +0800

    add the final two models

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
-
+from activitynetwork.models import Activity
 class VolunteerProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
@@ -10,3 +10,9 @@ class VolunteerProfile(UserenaBaseProfile):
                                 related_name='volunteer_profile')
     name = models.CharField(_('name'),
                                 max_length=20)
+    follow = models.ManyToManyField(Activity)
+
+class Comment(models.Model):
+	text = models.CharField(max_length=100)
+	author = models.ForeignKey(VolunteerProfile, related_name = 'Comment_author')
+	receiver = models.ForeignKey(VolunteerProfile, related_name = 'Comment_receiver') #the one who is reviewed
