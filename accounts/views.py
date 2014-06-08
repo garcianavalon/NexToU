86b3344074cda9c3369f699a6258f47bf457c1b5
ck520702002 <ck520702002@gmail.com>
2014-06-09 07:44:59 +0800

    fixed profile list

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from userena.views import ProfileListView
-from activitynetwork.models import talents
+from accounts.models import Talent
 # Create your views here.
 class CustomProfileListView (ProfileListView):
-	template_name = "profile_list.html"
 	def get_context_data(self, **kwargs):
 		context = super(CustomProfileListView, self).get_context_data(**kwargs)
-		context['talents_list'] = talents.objects.all()
+		context['talents_list'] = Talent.objects.all()
 		return context
