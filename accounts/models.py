76286695e7ff90aa0f1305a51e8b9f7e30e6a55c
Enrique Garcia <garcianavalon@gmail.com>
2014-06-09 07:49:13 +0800

    all activities list

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
 from activitynetwork.models import Activity
+class Talent(models.Model):
+    name = models.CharField(max_length=30)
+    def __unicode__(self): # __unicode__ on Python 2
+        return self.name
+
 class VolunteerProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
@@ -17,14 +22,9 @@ class VolunteerProfile(UserenaBaseProfile):
                                 max_length=140)
     resume = models.CharField(_('resume'),
                                 max_length=1000)
-    talents = models.ForeignKey(Talent)
+    #talents = models.ForeignKey(Talent)
 
     participated_activities = models.ManyToManyField(Activity,related_name = 'participated_activities')
-
-class Talent(models.Model):
-    name = models.CharField(max_length=30)
-    def __unicode__(self): # __unicode__ on Python 2
-        return self.name
 
 class Comment(models.Model):
 	text = models.CharField(max_length=100)
