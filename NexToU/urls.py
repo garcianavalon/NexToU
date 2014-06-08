60c1b6ac03cfbc6c0d5e2e71e82c5bb9a093b8ac
Enrique Garcia <garcianavalon@gmail.com>
2014-06-09 00:28:07 +0800

    Created index view

--- NexToU/urls.py
+++ NexToU/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-#from emailcontactform import views
+from views import IndexView
 from activitynetwork import views as act_views
 from accounts.forms import CustomSignupForm, CustomSigninForm
 from userena import views as userena_views
 urlpatterns = patterns('',
 
-    #url(r'^$', views.IndexView.as_view(), name='index'), #Index, first page
+    url(r'^$', IndexView.as_view(), name='landing'), #Index, first page
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/signup/$',
         userena_views.signup,
