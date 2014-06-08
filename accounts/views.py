48313bb6aaeb698997fab3ffc4b80a6a68a101d8
ck520702002 <ck520702002@gmail.com>
2014-06-09 07:29:29 +0800

    profile_list

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from userena.views import ProfileListView
+from activitynetwork.models import talents
 # Create your views here.
-class TalentsView(ListView):
-	model = VolunteerProfile
+class CustomProfileListView (ProfileListView):
 	template_name = "profile_list.html"
+	def get_context_data(self, **kwargs):
+		context = super(CustomProfileListView, self).get_context_data(**kwargs)
+		context['talents_list'] = talents.objects.all()
+		return context
