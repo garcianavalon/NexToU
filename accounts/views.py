04ace8efc905add0fb28f0d9546ecf0c32bcd161
Enrique Garcia <garcianavalon@gmail.com>
2014-06-09 10:58:55 +0800

    email contact form

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from userena.views import ProfileListView
 from accounts.models import Talent
 # Create your views here.
 class CustomProfileListView (ProfileListView):
-	def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
 		context = super(CustomProfileListView, self).get_context_data(**kwargs)
 		context['talents_list'] = Talent.objects.all()
 		return context
